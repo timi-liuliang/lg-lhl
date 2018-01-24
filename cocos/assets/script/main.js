af9a1026229356df0639ec6517540d76d6d3b697
liuliang <qq79402005@gmail.com>
2018-01-24 17:49:01 +0800

    #5 点击添加新的旋转块，根据prefab创建

--- cocos/assets/script/main.js
+++ cocos/assets/script/main.js
@@ -4,18 +4,23 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
+        // 父结点
+        houseParentNode : cc.Node,
+
+        // 房子预制体
+        housePrefab : cc.Prefab
     },
 
     // LIFE-CYCLE CALLBACKS:
 
     onLoad () {
-      // 开启物理系统
-      cc.director.getPhysicsManager().enabled = true;
+        // 开启物理系统
+        cc.director.getPhysicsManager().enabled = true;
 
-      cc.director.getPhysicsManager().debugDrawFlags = cc.PhysicsManager.DrawBits.e_aabbBit | cc.PhysicsManager.DrawBits.e_pairBit | cc.PhysicsManager.DrawBits.e_centerOfMassBit | cc.PhysicsManager.DrawBits.e_jointBit | cc.PhysicsManager.DrawBits.e_shapeBit;
-      //cc.director.getPhysicsManager().debugDrawFlags = 0;
+        cc.director.getPhysicsManager().debugDrawFlags = cc.PhysicsManager.DrawBits.e_aabbBit | cc.PhysicsManager.DrawBits.e_pairBit | cc.PhysicsManager.DrawBits.e_centerOfMassBit | cc.PhysicsManager.DrawBits.e_jointBit | cc.PhysicsManager.DrawBits.e_shapeBit;
+        //cc.director.getPhysicsManager().debugDrawFlags = 0;
 
-      this.registerInput();
+        this.registerInput();
     },
 
     start () {
@@ -49,7 +54,10 @@ cc.Class({
 
     // 放置房子
     dropHouse() {
-      
+        var newHouse = cc.instantiate(this.housePrefab);
+        newHouse.parent = this.houseParentNode;
+        newHouse.setScale( 0.7, 0.7);
+        newHouse.setPosition(cc.rand() % 500, 500);
     },
 
 });
