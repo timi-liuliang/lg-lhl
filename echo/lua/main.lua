3086925a4c5aade6ace8039eeab46fbf2670d586
LiuLiang <qq79402005@gmail.com>
2018-09-10 17:22:53 +0800

    move camera position

--- echo/lua/main.lua
+++ echo/lua/main.lua
@@ -2,12 +2,15 @@ local main = { }
 local houses = nil
 local ground = nil
 local bgs = nil
+local camera = nil
+local cameraYPos = 0.0
 
 -- start
 function main:start()
 	houses = self:getNode("houses")
 	bgs    = self:getNode("bgs")
 	ground = self:getNode("ground")
+	camera = self:getNode("camera")
 end
 
 -- update
@@ -16,6 +19,9 @@ function main:update()
 		local newHouse = Node.load("Res://scene/house.scene")
 		if newHouse~=nil then
 			houses:addChild(newHouse)
+			
+			cameraYPos = cameraYPos + 30.0
+			camera:setLocalPosition(vec3(0.0, cameraYPos, 0.0))
 		end
 	end
 end
